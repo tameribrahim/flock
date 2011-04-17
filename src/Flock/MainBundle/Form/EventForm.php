13b635a6a35bdae3ac27050d44d4597c3b2e9569
Amal Raghav <amal.raghav@gmail.com>
2011-04-17 11:03:28 +0530

    added css stylesheet and formalize

---
+++ src/Flock/MainBundle/Form/EventForm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by amalraghav <amal.raghav@gmail.com>
+ * Date: 17/04/11
+ */
+ 
+namespace Flock\MainBundle\Form;
+
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\TextField;
+use Symfony\Component\Form\TextareaField;
+use Symfony\Component\Form\CheckboxField;
+
+class EventForm extends Form
+{
+    
+}
